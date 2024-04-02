2d6094649d94ecea14fa172a3d31966addd2d8b9
pbenimet <pbenimet@uc.cl>
2024-04-02 15:58:17 -0300

    Primera versión de Viajes Chile

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$( window ).on( "resize",   function() 
+                            {$( "ancho" ).append( "<div>" + $( window ).width() + "</div>" );});
+                                
+
+    
+
+                                            
+
+
+
+
+       
